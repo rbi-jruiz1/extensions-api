5cebfc88ebc0e1a828ceb362230b38f1126d909d
Joseph Ruiz <jruiz1@rbi.com>
2025-05-07 10:45:29 -0400

    boilerplate

--- Samples-Typescript/Dashboard/PdfViewer/pdfViewer.tsx
+++
@@ -1,6 +0,0 @@
-import { PdfViewerComponent } from './pdfViewerComponent';
-
-// Wrap everything in an anonymous function to avoid polluting the global namespace
-(async () => {
-  await PdfViewerComponent.initialize();
-})();
--- Samples-Typescript/Dashboard/PdfViewer/pdfViewerDialog.tsx
+++
@@ -1,6 +0,0 @@
-import { PdfViewerDialogComponent } from './pdfViewerDialogComponent';
-
-// Wrap everything in an anonymous function to avoid polluting the global namespace
-(async () => {
-  await PdfViewerDialogComponent.initialize();
-})();
